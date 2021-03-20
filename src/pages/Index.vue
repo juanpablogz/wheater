51f0c9f9227563136c851493f3385d6ec6ac2517
juan pablo gomez zapata <juanpablogomezzapata@gmail.com>
2021-03-20 14:49:05 -0500

    delete

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -48,7 +48,7 @@ export default {
       var data = this;
       var xhr = new XMLHttpRequest();
       var url =
-        `http://api.openweathermap.org/data/2.5/forecast?q=bogota&units=metric&mode=json&appid=${WHEATER}`;
+        `http://api.openweathermap.org/data/2.5/forecast?q=bogota&units=metric&mode=json&appid=080ec8560a4965f95a43f80be54bae5d`;
       xhr.open("GET", url, false);
       xhr.onreadystatechange = function() {
         if (this.readyState === XMLHttpRequest.DONE) {
